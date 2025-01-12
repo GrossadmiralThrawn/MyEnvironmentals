aeedc42878251070dcc0c734825bb90a3508d768
Lukas <lukasd2000@gmx.de>
2025-01-12 14:40:26 +0100

    Anwendung der Farbschattierung.

--- app/src/main/java/com/myenvironmentals/ConnectionScreen.kt
+++ app/src/main/java/com/myenvironmentals/ConnectionScreen.kt
@@ -3,11 +3,14 @@ package com.myenvironmentals
 
 
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.myenvironmentals.models.connections.IConnection
@@ -20,11 +23,16 @@ import com.myenvironmentals.viewmodels.AddMicrocontrollerViewModel
 @Composable
 fun ConnectionScreen(addMicrocontrollerViewModel: AddMicrocontrollerViewModel)
 {
+    val fontColor by addMicrocontrollerViewModel.fontColor.collectAsState()
+    val bodyColor by addMicrocontrollerViewModel.bodyColor.collectAsState()
+
+
+
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize().background(bodyColor)
     ) {
-        Text(text = "Verbindung: ${addMicrocontrollerViewModel.getConnectionType()::class.simpleName}")
+        Text(text = "Verbindung: ${addMicrocontrollerViewModel.getConnectionType()::class.simpleName}", color = fontColor)
     }
 }
